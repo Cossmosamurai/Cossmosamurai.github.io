6fa1da9d20d6c27fa43457db4fe1e9b3818def8b
Cossmosamurai <61940545+Cossmosamurai@users.noreply.github.com>
2020-05-10 16:59:56 +0200

    Check wrap-menu

--- js/main.js
+++ js/main.js
@@ -1,212 +1,29 @@
-$(function () {
 
-		$(window).scroll(function() {
-	    $('.about_2011').each(function(){
-	        var imagePos = $(this).offset().top;
 
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInLeft");
-	        }
-	    });
-	});
 
-		$(window).scroll(function() {
-	    $('.about_2012').each(function(){
-	        var imagePos = $(this).offset().top;
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
 
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInRight");
-	        }
-	    });
-	});
+let body = document.querySelector('body');
 
-		$(window).scroll(function() {
-	    $('.about_2013').each(function(){
-	        var imagePos = $(this).offset().top;
+if(isMobile.any()){
 
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInLeft");
-	        }
-	    });
-	});
+		body.classList.add('touch');
 
-		$(window).scroll(function() {
-	    $('.about_2014').each(function(){
-	        var imagePos = $(this).offset().top;
+} else {
 
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInRight");
-	        }
-	    });
-	});
+		body.classList.add('mouse');
 
-		$(window).scroll(function() {
-	    $('.offer-title').each(function(){
-	        var imagePos = $(this).offset().top;
+}
 
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInRight");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.offer-text').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInLeft");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.mark').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInUp");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.justin').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInUp");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.antonio').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInUp");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.service-title').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("zoomIn");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.service-text').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInUp");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.graph').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInLeft");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.heart').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInUp");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.cloud').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInRight");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.btn').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("zoomIn");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.phone').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInLeft");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.adress').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInUp");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.mail').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("fadeInRight");
-	        }
-	    });
-	});
-
-		$(window).scroll(function() {
-	    $('.input').each(function(){
-	        var imagePos = $(this).offset().top;
-
-	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
-	            $(this).addClass("slideInLeft");
-	        }
-	    });
-	});
-
-	})
+$('.gamburger-menu').on('click', function (e) {
+	e.preventDefault();
+	$('.menu-btn').toggleClass('btn-active');
+	$('.header').toggleClass('menu-active');
+})
